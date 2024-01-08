b6b1d9f7316f614202178730b45deb62dface8b3
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2024-01-08 18:49:00 +0100

    contact form, react scroll

--- 1223-portfolio/src/components/Navbar.jsx
+++ 1223-portfolio/src/components/Navbar.jsx
@@ -3,13 +3,16 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import Logo from '../assets/logo1.png'
+import {Link} from 'react-scroll'
+
 const Navbar = () => {
 
     const[nav, setNav]  = useState(false);
     const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#292524]  text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 
+                bg-[#292524]  text-gray-300 border-b-[#44403c] border-b-2'>
         <div>
             <img src={Logo} alt="Logo Image" style={{width: '50px'}} />
         </div>
@@ -17,19 +20,44 @@ const Navbar = () => {
         {/* Menu */}
         <ul className='hidden md:flex justify-center'>
             <li className="my-auto text-center hover:bg-pink-600">
+            <Link 
+                to="home" 
+                smooth={true} 
+                duration={500}>
                 Home
+                </Link>
             </li>
             <li className='hover:bg-pink-600'>
-                About
-            </li>
-            <li>
+            <Link 
+                to="skills" 
+                smooth={true} 
+                duration={500}>
                 Skills
+                </Link>
             </li>
             <li>
+            <Link 
+                to="skills" 
+                smooth={true} 
+                duration={500}>
+                Skills
+                </Link>
+            </li>
+            <li>
+            <Link 
+                to="work" 
+                smooth={true} 
+                duration={500}>
                 Work
+                </Link>
             </li>
             <li>
+            <Link 
+                to="contact" 
+                smooth={true} 
+                duration={500}>
                 Contact
+                </Link>
             </li>
         </ul>
         
@@ -42,19 +70,49 @@ const Navbar = () => {
         {/* Mobile Menu */}
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
             <li className='py-6 text-4xl'>
+            <Link 
+                onClick={handleClick}
+                to="home" 
+                smooth={true} 
+                duration={500}>
                 Home
+                </Link>
             </li>
             <li className='py-6 text-4xl'>
-                About
-            </li>
-            <li className='py-6 text-4xl'>
+            <Link 
+                onClick={handleClick}
+                to="skills" 
+                smooth={true} 
+                duration={500}>
                 Skills
+                </Link>
             </li>
             <li className='py-6 text-4xl'>
+            <Link 
+                onClick={handleClick}
+                to="skills" 
+                smooth={true} 
+                duration={500}>
+                Skills
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+            <Link 
+                onClick={handleClick}
+                to="work" 
+                smooth={true} 
+                duration={500}>
                 Work
+                </Link>
             </li>
             <li className='py-6 text-4xl'>
+            <Link 
+                onClick={handleClick}
+                to="contact" 
+                smooth={true} 
+                duration={500}>
                 Contact
+                </Link>
             </li>
         </ul>
 
