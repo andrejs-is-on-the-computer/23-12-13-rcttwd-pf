0ea3e4e332808eb08e830c780777af9acd52106a
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2024-01-08 12:58:05 +0100

    Update Home.jsx

--- 1223-portfolio/src/components/Home.jsx
+++ 1223-portfolio/src/components/Home.jsx
@@ -6,16 +6,17 @@ const Home = () => {
     <div name="home" className='w-full h-screen bg-[#44403c]'>
         
         {/* Container */}
-        <div className='max-w-[1000px] mx-auto px-8 ml-10 flex flex-col justify-center h-full'>
-            <p className='text-pink-600'>Hi, my name is</p>
-            <h1 className='text-4xl sm:text-7xl font-bold py-1 text-[#ccd6f6] w-auto
+        <div className='max-w-[1000px] mx-auto px-12 ml-10 flex flex-col justify-center h-full'>
+            <p className='text-pink-600 select-none'>Hi, my name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold py-1 text-[#ccd6f6] select-none
                          hover:bg-pink-600 hover:text-[#1c1917] duration-1000'>
                 Andrejs Bicevskis
             </h1>
-            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0] select-none
+                            hover:bg-pink-600 hover:text-[#1c1917] duration-1000'>
                 Front End Developer.
             </h2>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>
+            <p className='text-[#8892b0] py-4 max-w-[700px] select-none'>
                 I'm a front-end developer...
             </p>
             <div>
